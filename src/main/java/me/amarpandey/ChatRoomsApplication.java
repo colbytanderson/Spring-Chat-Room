bf1ca5b7ac1d1ca6478958ad3188a5f54f9636f6
Colby Anderson <colby-anderson@colbys-mbp.home>
2020-08-06 19:49:57 -0700

    moving from old acct

---
+++ src/main/java/me/amarpandey/ChatRoomsApplication.java
@@ -0,0 +1,12 @@
+package me.amarpandey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRoomsApplication.class, args);
+	}
+}
